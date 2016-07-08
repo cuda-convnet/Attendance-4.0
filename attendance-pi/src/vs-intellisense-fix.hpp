35b2a7a37a48f025bb5f7dd1edc98f3aae6fa54c
erikuhlmann <erikuhlmann@gmail.com>
2016-07-08 19:48:46 -0400

    More VS setup

--- attendance-pi/src/vs-intellisense-fix.hpp
+++ attendance-pi/src/vs-intellisense-fix.hpp
@@ -1,3 +1,6 @@
+// in nmake projects, Visual Studio's IntelliSense doesn't know what C++ version we're on
+// so we make sure it does after checking if IntelliSense is currently parsing
+
 #if defined(__INTELLISENSE__)
 #undef __cplusplus
 #define __cplusplus 201103L
